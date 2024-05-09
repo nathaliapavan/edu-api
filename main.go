9b4244a151144dcbfcef55697707c028a69d0943
Nathalia Pavan <nanahpavan@gmail.com>
2024-05-09 15:38:25 -0300

    feat: add env config (#5)
    Co-authored-by: pavan <pavan@pavan.pavan>

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nathaliapavan/edu-api/api"
+	"github.com/nathaliapavan/edu-api/infra/config"
 )
 
 func main() {
 	gin.ForceConsoleColor()
-	service := api.NewService()
-	service.Start()
+
+	if err := config.StartConfig(); err != nil {
+		log.Fatalf("error loading .env: %v", err)
+	}
+
+	if err := api.NewService().Start(); err != nil {
+		log.Fatal(err)
+	}
 }
